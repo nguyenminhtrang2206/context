92a87dc9f1cbea0bab166ef1b6d3dfcde1115e6e
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2022-02-07 12:27:56 +0200

    adding todo folder

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import AddTodo from "./components/Todo/AddTodo";
 import TodoList from "./components/Todo/TodoList";
 
+import { Provider } from "./components/store/todoStore";
 const App = () => {
   return (
     <div>
-      <AddTodo />
-      <TodoList />
+      <Provider>
+        <AddTodo />
+        <TodoList />
+      </Provider>
     </div>
   );
 };
--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { notes } from "../../notes";
 import classes from "./TodoList.module.css";
+import { NotesContext } from "../store/todoStore";
 
 const TodoList = () => {
+  const context = useContext(NotesContext);
+  console.log(context);
+
   return (
     <div className={classes.todos}>
       <h1>Notes:</h1>
-      {notes.map((note) => {
+      {context.notes.map(note => {
         return (
-          <div className={classes.todo}>
+          <div className={classes.todo} key={note.id}>
             <h2>
               {note.id}. {note.title}
             </h2>
