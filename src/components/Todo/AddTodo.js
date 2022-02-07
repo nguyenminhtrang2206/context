0bddef38f924dd7dcbf647c68222dc90edcf7d37
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2022-02-07 12:08:18 +0200

    second commit

---
+++ src/components/Todo/AddTodo.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import classes from "./AddTodo.module.css";
+import Button from "../UI/Button";
+
+const AddTodo = ({ addHandler }) => {
+  const [todo, setTodo] = useState({ title: "", task: "" });
+
+  const changeHandler = e => {
+    const { name, value } = e.target;
+
+    setTodo(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(todo);
+  };
+
+  return (
+    <form onSubmit={addHandler} className={classes.input}>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input onChange={changeHandler} type="text" id="title" name="title" />
+      </div>
+      <div>
+        <label htmlFor="task">Task</label>
+        <input onChange={changeHandler} type="text" id="task" name="task" />
+      </div>
+      <Button type="submit">Add Task</Button>
+    </form>
+  );
+};
+
+export default AddTodo;
